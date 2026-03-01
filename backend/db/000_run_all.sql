aff14f722127640521088651c3a5a7c1056b882f
Haky <dinhkhanghuy2061@gmail.com>
2026-03-01 01:37:10 -0800

    comment out 004.sql schema

--- backend/db/000_run_all.sql
+++ backend/db/000_run_all.sql
@@ -2,7 +2,7 @@
 \ir 001_auth_schema.sql
 \ir 002_categories_schema.sql
 \ir 003_transactions_schema.sql
-\ir 004_seed_dev_admin.sql
+--\ir 004_seed_dev_admin.sql
 \ir 005_budget_limits_schema.sql
 \ir 006_smart_budget_allocation.sql
 \ir 007_reports_indexes.sql
